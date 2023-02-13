4b0645623bf3a994a221de2be01849570968da61
Jenny Waller <113125376+jenmwa@users.noreply.github.com>
2023-02-13 17:50:16 +0100

    Add files via upload

---
+++ order/lime/js/site.js
@@ -0,0 +1,86 @@
+"use strict";
+
+$(document).ready(function () {
+	/* Video Lightbox */
+	if (!!$.prototype.simpleLightboxVideo) {
+		$('.video').simpleLightboxVideo();
+	}
+
+	/*ScrollUp*/
+	if (!!$.prototype.scrollUp) {
+		$.scrollUp();
+	}
+
+	/*Responsive Navigation*/
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-trigger span").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$(this).removeClass("open");
+		} else {
+			$("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+			$(this).addClass("open");
+		}
+	});
+
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-mobile ul a").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$("#nav-trigger span").removeClass("open");
+		}
+	});
+
+	/* Sticky Navigation */
+	if (!!$.prototype.stickyNavbar) {
+		$('#header').stickyNavbar();
+	}
+
+	$('#content').waypoint(function (direction) {
+		if (direction === 'down') {
+			$('#header').addClass('nav-solid fadeInDown');
+		}
+		else {
+			$('#header').removeClass('nav-solid fadeInDown');
+		}
+	});
+
+});
+
+
+/* Preloader and animations */
+$(window).load(function () { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow-y': 'visible'});
+
+	/* WOW Elements */
+	if (typeof WOW == 'function') {
+		new WOW().init();
+	}
+
+	/* Parallax Effects */
+	if (!!$.prototype.enllax) {
+		$(window).enllax();
+	}
+
+});
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
